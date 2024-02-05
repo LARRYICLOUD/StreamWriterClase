280507b1e8463fd7e81f7f1670840cf1ce5e2790
LARRYSALSA <75591878+LARRYICLOUD@users.noreply.github.com>
2024-02-04 22:07:55 -0500

    Agregar archivos de proyecto.

---
+++ StreamWriterClase/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamWriterClase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("YO SOY UN PROGRAMADOR Y EXPERTO EN REDES EXCELENTE");
+
+            /*//Creamos un archivo
+            FileStream fs = File.Create("Archivo con file.txt");
+
+            //Cerramos el flujo
+            fs.Close();*/
+
+            //Instanciamos a SW y le mandamos el archvo a su constructor
+            StreamWriter sw = new StreamWriter("Archivo con file.txt", true);
+
+            //Write / WriteLine
+            sw.WriteLine(" otro texto anexado");
+
+            sw.Close();
+        }
+    }
+}
